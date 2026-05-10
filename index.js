c816095e56a6ed29ac98c477cbbfb31985e98037
scorfi-max <scorfi0819@gmail.com>
2026-05-11 01:51:53 +0900

    remove firebase-key.json dependency

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ admin.initializeApp({
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
   })
 });
-
+process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
 const db = admin.firestore();
 
 async function test() {
